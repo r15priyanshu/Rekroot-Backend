61d73529c495e49512f9ec051b969fe43e2cfd46
Priyanshu Anand <anandpriyanshu6@gmail.com>
2026-05-30 17:46:20 +0530

    Initial commit with basic project structure.

---
+++ src/main/java/com/anshuit/rekroot/RekrootApplication.java
@@ -0,0 +1,11 @@
+package com.anshuit.rekroot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RekrootApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RekrootApplication.class, args);
+    }
+}
